85dba43cf7a2ea25c7b4da67fc3322ab7becdb32
Dario Seminara <darios3@gmail.com>
2012-01-30 11:49:56 -0300

    first commit: rails3.1.3 application created from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthTest::Application.config.secret_token = 'd819fa96f27f45eec6f8e1aa2b9209759463f4e34c40966cafc49f3e090da24852f48ad4059154b6d6e819ecfbc0b0403bff350abf491c299d30b6375ea3e6e6'
