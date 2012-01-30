43017df5b520a9925992fd4bdba01926ef4484f9
Dario Seminara <darios3@gmail.com>
2012-01-30 12:11:55 -0300

    created user model mock

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  class UserMock
+    def client_applications
+      ClientApplication
+    end    
+  end
+  
+  def current_user
+    UserMock.new
+  end
+  
   def login_required
     # do nothing
   end
