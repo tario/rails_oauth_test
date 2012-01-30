7aff9b78c4b886a84431edaa820322e1b6146256
Dario Seminara <dario3@gmail.com>
2012-01-30 12:22:46 -0300

    added rack middleware to allow consuming oauth

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'oauth/rack/oauth_filter'
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -44,5 +46,8 @@ module OauthTest
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.middleware.use OAuth::Rack::OAuthFilter
+
   end
 end
