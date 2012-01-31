8445a7ce05e61baaa950ef5a62f5f0184b90decf
Dario Seminara <dario3@gmail.com>
2012-01-31 09:33:52 -0300

    added extra script to show as connect to the oauth server provider

---
+++ extras/consumerapp.rb
@@ -0,0 +1,32 @@
+require "oauth"
+
+request_token = nil
+
+    
+callback_url = "<could be anything, only used by websites>"
+key= "<insert the consumer key here>"
+secret= "<insert the secret here>"
+consumer = OAuth::Consumer.new(key,secret, :site => "<insert site url here>")
+ 
+if ARGV.size <= 0
+  
+  print "request new access token from site...\n"
+ 
+  request_token = consumer.get_request_token(:oauth_callback => callback_url)
+  print "token: #{request_token.token}\n"
+  print "secret: #{request_token.secret}\n"
+  
+  print "authorize_url: #{request_token.authorize_url}\n"
+  gets
+else
+  print "using token=#{ARGV[0]} secret=#{ARGV[1]}...\n"
+
+  request_token = OAuth::RequestToken.new(consumer, ARGV[0], ARGV[1])
+end
+
+
+access_token = request_token.get_access_token
+
+print "access_token.token: #{access_token.token}\n"
+print "access_token.secret: #{access_token.secret}\n"
+
